dee9a234eab55c0cced70cb733eeb5eb8b5b9ff4
Ivan Gonzalez <ivansegundo702@gmail.com>
2024-03-27 14:46:14 -0600

    Add layout to pages of about of project

--- resources/views/about/background.blade.php
+++ resources/views/about/background.blade.php
@@ -3,5 +3,14 @@
 @section('title', 'Antecedentes del proyecto')
 
 @section('content')
-    //
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <h1 class="text-center pt-5 pb-3 h1">Contrinuyentes del proyecto</h1>
+            <article>
+                {{-- aqui empiezas a poner todo --}}
+            </article>
+        </div>
+    </div>
+</div>
 @endsection
