f19ee6c1eef74aeabf6a61420f2afe74b7f5f647
Ivan Gonzalez <ivansegundo702@gmail.com>
2024-05-03 16:30:24 -0600

    Avances de detalles

--- resources/views/about/background.blade.php
+++ resources/views/about/background.blade.php
@@ -8,7 +8,13 @@
         <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12">
             <h1 class="text-center pt-3 pb-3 mb-4 h1 border-bottom fw-light">Antecedentes del proyecto</h1>
             <article>
-                {{-- aqui empiezas a poner todo --}}
+                <div class="mensaje__section">
+                    <i class="fa-solid fa-screwdriver-wrench"></i>
+                    <div class="mensaje__contenido">
+                        <h1>Página en Construcción</h1>
+                        <p>¡Gracias por visitarnos! Estamos trabajando en esta página y pronto estará disponible con contenido interesante. Por favor, vuelva más tarde.</p>
+                    </div>
+                </div>
             </article>
         </div>
     </div>
